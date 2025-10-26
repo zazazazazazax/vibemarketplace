a719178b0923f571502ccf8dbf3f53dd57364003
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-27 00:57:58 +0100

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Fix MetaMask SDK: Ignora React Native deps
-      config.resolve.fallback = {
-        ...config.resolve.fallback || {},
-        '@react-native-async-storage/async-storage': false,
-      };
-    }
-    // Fix Pino/WC logger: Esternalizza pino-pretty (non bundle in serverless)
-    config.externals = [...(config.externals || []), 'pino-pretty'];
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
