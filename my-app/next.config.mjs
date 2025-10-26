97ef36d52df88657536e8bb12102620e7827e6c1
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-27 00:21:57 +0100

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,8 +2,9 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Fix per MetaMask/RainbowKit: Ignora React Native deps (solo client)
       config.resolve.fallback = {
-        ...config.resolve.fallback,
+        ...config.resolve.fallback || {},
         '@react-native-async-storage/async-storage': false,
         'pino-pretty': false,
       };
