4f4b78b8ed2b062c6a596c454b9e6f8405c9195e
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-27 12:07:53 +0100

    reroll next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Merger unico per fallback (fix doppio override)
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        '@react-native-async-storage/async-storage': false,
+        'pino-pretty': false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
