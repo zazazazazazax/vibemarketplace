c1020731571d4904169656cb7fbc7d45418c5c92
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-28 11:51:46 +0100

    Create wagmi.js

---
+++ my-app/lib/wagmi.js
@@ -0,0 +1,9 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { base } from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'Vibe.Market',
+  projectId: '8e4f39df88b73f8ff1e701f88b4fea0c', // Il tuo ID
+  chains: [base],
+  ssr: true, // Obbligatorio per Next.js SSR
+});
