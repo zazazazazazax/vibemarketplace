4191bf7036494cc359eff4ef9550aeb9da7efffb
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-28 18:58:47 +0100

    Update wagmi.js

--- my-app/src/lib/wagmi.js
+++ my-app/src/lib/wagmi.js
@@ -1,44 +1,9 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { base } from 'wagmi/chains';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-  metaMaskWallet,
-  rainbowWallet,
-  coinbaseWallet,
-  walletConnectWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-
-const projectId = '8e4f39df88b73f8ff1e701f88b4fea0c';
-
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended',
-      wallets: [
-        rainbowWallet({ projectId }),
-        metaMaskWallet({ projectId }),
-      ],
-    },
-    {
-      groupName: 'Other',
-      wallets: [
-        coinbaseWallet({
-          appName: 'Vibe.Market',
-        }),
-        walletConnectWallet({ projectId }),
-      ],
-    },
-  ],
-  {
-    appName: 'Vibe.Market',
-    projectId,
-  }
-);
 
 export const config = getDefaultConfig({
   appName: 'Vibe.Market',
-  projectId,
+  projectId: '8e4f39df88b73f8ff1e701f88b4fea0c',
   chains: [base],
-  connectors,
   ssr: true, // Obbligatorio per Next.js SSR
 });
