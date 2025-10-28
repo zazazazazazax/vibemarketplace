d4d1e75a57a9e968065a8072e4204f8407012009
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-10-28 02:42:52 +0100

    Update next.config.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://*.walletconnect.org wss://relay.walletconnect.org https://api.web3modal.org https://cca-lite.coinbase.com https://*.coinbase.com https://raw.githubusercontent.com https://*.githubusercontent.com; img-src 'self' https://*.githubusercontent.com https://*.coinbase.com data: blob: *;", // FIX: Aggiunto wss://relay.walletconnect.org per WS WC, img-src espanso per icone GitHub
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://*.walletconnect.org wss://relay.walletconnect.org https://pulse.walletconnect.org https://api.web3modal.org https://cca-lite.coinbase.com https://*.coinbase.com https://raw.githubusercontent.com https://*.githubusercontent.com; img-src 'self' data: blob: https://phantom.app https://rainbow.me https://metamask.io https://www.coinbase.com https://walletconnect.com *;", // FIX: img-src per favicon.ico domini
           },
         ],
       },
