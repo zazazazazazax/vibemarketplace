66714cee41db891deca34061412ce1859977e042
zazazazazazax <35748889+zazazazazazax@users.noreply.github.com>
2025-11-25 20:58:20 +0100

    Update page.js

--- my-app/src/app/binders/page.js
+++ my-app/src/app/binders/page.js
@@ -976,7 +976,7 @@ const [price, isEth, currency] = await readContract(config, {
   </div>
 )}
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full miniapp-grid">
               {currentListings.length > 0 ? (
                 currentListings.map((listing) => {
                   const cacheKey = `${listing.tokenId}-${listing.collection}`;
